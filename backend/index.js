e3e0267425c2a8153a5d9a7d29faab4a43ecfe90
Mithelesh <mitheleshv@gmail.com>
2024-04-20 23:00:49 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express, { request, response } from 'express';
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    method: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-type']
+}))
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('MERN Project')
+});
+
+app.use('/books', booksRoute)
+
+mongoose
+.connect(mongoDBURL)
+.then((result) => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    });
+    }).catch((err) => {
+        console.log(err)
+    });
---
+++ frontend/src/pages/EditBook.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSnackbar } from "notistack";
+
+const EditBook = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [publishYear, setPublishYear] = useState('');
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { enqueueSnackbar } = useSnackbar();
+
+    useEffect(() => {
+        // setLoading(true);
+        // axios
+        //     .get(`http://localhost:5555/books/${id}`)
+        //     .then((response) => {
+        //         setAuthor(response.data.author)
+        //         setTitle(response.data.title)
+        //         setPublishYear(response.data.publishYear)
+        //         setLoading(false)
+        //         console.log('hello')
+        //     })
+        //     .catch((error) => {
+            //         alert('error occured')
+            //         console.log(error)
+            //     })
+            fetchBook()
+    }, [])
+
+    const fetchBook = async () => {
+        const response = await fetch(`http://localhost:5555/books/${id}`)
+        const data = await response.json()
+        setAuthor(data.author)
+        setTitle(data.title)
+        setPublishYear(data.publishYear)
+
+    }
+
+    const handleEditBook = () => {
+        const data = {
+            title,
+            author,
+            publishYear
+        };
+        setLoading(true);
+        axios
+            .put(`http://localhost:5555/books/${id}`, data)
+            .then(() => {
+                setLoading(false);
+                enqueueSnackbar('Book Edited Sucessfully', { variant: 'success' })
+                navigate('/')
+            })
+            .catch((error) => {
+                setLoading(false)
+                alert('Error Occured... Check Console');
+                enqueueSnackbar('Error', { variant: 'error' })
+                console.log(error)
+            })
+    }
+    return (
+        <div className='p-4'>
+            <BackButton />
+            <h1 className="text-3xl my-4">Edit Book</h1>
+            { loading ? <Spinner /> : '' }
+            <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600] p-4 mx-auto">
+                <div className="my-4">
+                    <label htmlFor="" className="text-xl mr-4 text-grey-500">Title</label>
+                    <input type="text" name="" id="" value={title} onChange={(e) => {setTitle(e.target.value); console.log(e.target.value)}} className='border-2 border-grey-500 px-4 py-2 w-full' />
+                </div>
+                <div className="my-4">
+                    <label htmlFor="" className="text-xl mr-4 text-grey-500">Author</label>
+                    <input type="text" name="" id="" value={author} onChange={(e) => {setAuthor(e.target.value)}} className='border-2 border-grey-500 px-4 py-2 w-full' />
+                </div>
+                <div className="my-4">
+                    <label htmlFor="" className="text-xl mr-4 text-grey-500">Publish Year</label>
+                    <input type="text" name="" id="" value={publishYear} onChange={(e) => {setPublishYear(e.target.value)}} className='border-2 border-grey-500 px-4 py-2 w-full' />
+                </div>
+                <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
+                    Save
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default EditBook
