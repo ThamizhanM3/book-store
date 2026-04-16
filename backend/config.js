310b7169333005e90dc7b365d7950017bd3f41b9
Thamizhan M3 <mithelesh.tmz.m3@gmail.com>
2026-04-16 13:33:27 +0530

    vite env

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
+// export const mongoDBURL = 'mongodb+srv://Mithelesh:MitheleshM3@mithelesh01.yapjwgo.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Mithelesh01'
 export const mongoDBURL = 'mongodb+srv://Mithelesh:MitheleshM3@mithelesh01.yapjwgo.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Mithelesh01'
--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.use(cors({
     origin: '*',
-    method: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-type']
 }))
 
