bf894a70152f8f1019e259d9f1d089639f7ec499
Thamizhan M3 <mithelesh.tmz.m3@gmail.com>
2026-04-17 11:06:21 +0530

    frontend dockerfile v5

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = window.__env__?.VITE_APP_API_URL || 'http://localhost:5555'
+
+const API_URL = window.__ENV__?.VITE_APP_API_URL ?? import.meta.env.VITE_APP_API_URL ?? "http://localhost:5555";
+
 
 const API = axios.create({
     // baseURL: import.meta.env.VITE_APP_API_URL,
