98964d1ad3e4ab3a6645fd120b0676048e3cd60a
Thamizhan M3 <mithelesh.tmz.m3@gmail.com>
2026-04-18 16:12:51 +0530

    feat!: backend pipeline v16 \

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,4 @@ export const PORT = 5555;
 
 export const mongoDBURL = process.env.MONGO_URI
 // export const mongoDBURL = 'mongodb+srv://Mithelesh:<password>@mithelesh01.yapjwgo.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Mithelesh01'
-// test5
+// test10
--- frontend/pipeline-test.js
+++ frontend/pipeline-test.js
@@ -13,3 +13,4 @@ console.log('test 6');
 console.log('test 5');
 console.log('test 4');
 console.log('test 3');
+console.log('test 2');
